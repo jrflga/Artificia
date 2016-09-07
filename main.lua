53b984359bc1fe4f241a1140533d4a8ebff8877a
João Ricardo <jrflga@gmail.com>
2016-09-07 19:09:39 -0300

    Urgh :koala:

--- main.lua
+++ main.lua
@@ -1,15 +1,26 @@
-local Tilemap = require "tilemap"
+local Renderer = require "renderer"
+local LoopManager = require "loop_manager"
+local Tilemap = require "tile_map"
 
+renderer = Renderer:create()
+loopmngr = LoopManager:create()
 tilemap = Tilemap:create()
+
+gTime = 0
 
 function love.load()
 	tilemap:load()
+
+	font = love.graphics.newFont("gfx/font.ttf", 32)
+	love.graphics.setFont(font)
 end
 
 function love.update(dt)
-	tilemap:update(dt)
+	gTime = gTime + dt
+	loopmngr:update(dt)
 end
 
 function love.draw()
-	tilemap:draw()
+	love.graphics.print (love.timer.getFPS (), 10, 10)
+	renderer:draw()
 end
