91b378d6032d01868f828da4e504d91975041f50
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2016-09-12 07:28:43 -0300

    Fix indenting, add current mode besides mode options

--- main.lua
+++ main.lua
@@ -4,11 +4,12 @@ local Renderer = require "renderer"
 local LoopManager = require "loop_manager"
 local Tilemap = require "tile_map"
 
-local mapsizeX, mapsizeY = 64, 32
+local mapsizeX, mapsizeY = 64, 32  -- TODO: Min 4x4, Max 64x32 -> User must pick size
 local initPosX, initPosY = 170, 60
 local tileSize = 16
 
-clickmode = "Pick a mode (1 - Wall, 2 - Point A and 3 - Point B)"
+modeoptions = "Pick a mode (1 - Clear, 2 - Wall, 3 - Point A and 4 - Point B)"
+clickmode = "Clear"
 tipColor = nil
 
 renderer = Renderer:create()
@@ -18,31 +19,35 @@ tilemap = Tilemap:create(initPosX, initPosY, mapsizeX, mapsizeY, tileSize)
 gTime = 0
 
 function love.load()
-	tilemap:load()
+  tilemap:load()
 
-	font = love.graphics.newImageFont("gfx/font.png", " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/():;%&`'*#=[]\"")
-	love.graphics.setFont(font)
+  font = love.graphics.newImageFont("gfx/font.png", " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/():;%&`'*#=[]\"")
+  love.graphics.setFont(font)
 
-	tipColor =  lue:newColor():setColor({50, 50, 50})
+  tipColor = lue:newColor():setColor({50, 50, 50})
 end
 
 function love.update(dt)
-	gTime = gTime + dt
-	print(gTime)
-	love.window.setTitle("Artificia - FPS" .. love.timer.getFPS())
-	loopmngr:update(dt)
-	lue:update(dt)
+  gTime = gTime + dt
+  --print(gTime)
+  love.window.setTitle("Artificia - " .. love.timer.getFPS() .. " FPS")
+  loopmngr:update(dt)
+  lue:update(dt)
 end
 
 function love.draw()
-	renderer:draw()
-	love.graphics.setColor(tipColor:getColor())
-	love.graphics.print(clickmode, 10, 10)
+  renderer:draw()
+  love.graphics.setColor({50, 50, 50})
+  love.graphics.print(modeoptions, 10, 10)
+  love.graphics.setColor(tipColor:getColor())
+  love.graphics.print(clickmode, 10, 30)
 end
 
 function love.keypressed(k)
-   if k == "escape" then love.event.quit() end
-	 if k == "1" then clickmode = "Wall" tipColor:setColor({50, 50, 100}) end
-	 if k == "2" then clickmode = "Point_A" tipColor:setColor({255, 0, 0}) end
-	 if k == "3" then clickmode = "Point_B" tipColor:setColor({0, 255, 0}) end
+  if k == "escape" then love.event.quit() end
+  if k == "1" then clickmode = "Clear"   tipColor:setColor({50, 50, 50}) end
+  if k == "2" then clickmode = "Wall"    tipColor:setColor({65, 65, 75})   end
+  if k == "3" then clickmode = "Water"   tipColor:setColor({52, 152, 219}) end
+  if k == "4" then clickmode = "Point A" tipColor:setColor({200, 50, 50})  end
+  if k == "5" then clickmode = "Point B" tipColor:setColor({50, 200, 50})  end
 end
